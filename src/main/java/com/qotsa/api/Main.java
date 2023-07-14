014c5796299d00456fc4f39c6a2776d65389c0ff
Owen Lambert <OwenLambert98@hotmail.com>
2023-07-14 01:15:05 +0100

    Tidy up

--- src/main/java/com/qotsa/api/Main.java
+++ src/main/java/com/qotsa/api/Main.java
@@ -9,20 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
-  /**
-   * The entry point of the Queens of the Stone Age API application.
-   *
-   * <p>
-   * This method starts the Spring Boot application by invoking the
-   * {@link SpringApplication#run(Class, String...)}
-   * method with the {@link Main} class and command-line arguments.
-   * It initializes the application and starts the embedded server
-   * to handle HTTP requests.
-   * </p>
-   *
-   * @param args The command-line arguments passed to the application.
-   */
-  public final void main(final String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
   }
 }
